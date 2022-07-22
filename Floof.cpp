3f47fdd4c58c2cbea38b4753df32bee992a60f61
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-07-22 19:24:46 +0200

    Added glfw submodule

--- Floof.cpp
+++ Floof.cpp
@@ -1,10 +1,35 @@
-﻿#include <iostream>
-#include <vulkan/vulkan.h>
+﻿#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-#include "Floof.h"
-#include "Utils/Utils.h"
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
-int main() 
-{
-	SuperUsefullFunction();
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
