d860dcdc9471da13aa8a6e2fdac2b5e95896991f
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-07-27 17:07:45 +0200

    Removed x86 builds.

--- Source/Floof.h
+++ Source/Floof.h
@@ -5,7 +5,11 @@
 
 namespace FLOOF {
 #define LOG(msg, ...) std::cout << std::format(msg, __VA_ARGS__)
-
+#ifndef NDEBUG
 #define ASSERT(expr, msg, ...) do { if (!(expr)) { LOG(msg, __VA_ARGS__); __debugbreak(); } } while (false)
+#else
+#define ASSERT(expr, msg, ...)
+#endif // !NDEBUG
+
 }
 
