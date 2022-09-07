36ad6245747ae6c736901125af80dbb979f59114
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-09-07 14:04:41 +0200

    Parser for visim format.

--- Source/Utils.cpp
+++ Source/Utils.cpp
@@ -6,14 +6,13 @@
 
 namespace FLOOF {
 	namespace Utils {
-		std::pair<std::vector<Vertex>, std::vector<uint32_t>> GetVisimVertexData(const std::string& path) {
+		std::vector<Vertex> GetVisimVertexData(const std::string& path) {
 			std::vector<Vertex> vertexData;
-			std::vector<uint32_t> indexData;
 
 			std::ifstream file(path);
 			if (!file.is_open()) {
 				std::cout << "Cant open file: " << path << std::endl;
-				return { vertexData, indexData };
+				return vertexData;
 			}
 
 			uint32_t vertexCount{}; // Throwaway variabel.
@@ -28,12 +27,35 @@ namespace FLOOF {
 
 			for (uint32_t i = 2; i < tempVertexData.size(); i += 3) {
 				Vertex v{};
-				v.pos.z = tempVertexData[i - 2];
+				v.pos.x = tempVertexData[i - 2];
 				v.pos.y = tempVertexData[i - 1];
-				v.pos.x = tempVertexData[i - 0];
+				v.pos.z = tempVertexData[i - 0];
+				vertexData.push_back(v);
 			}
 
-			return { vertexData, indexData };
+			for (uint32_t i = 2; i < vertexData.size(); i += 3) {
+				Vertex& a = vertexData[i - 2];
+				Vertex& b = vertexData[i - 1];
+				Vertex& c = vertexData[i - 0];
+
+				glm::vec3 ab = b.pos - a.pos;
+				glm::vec3 ac = c.pos - a.pos;
+
+				glm::vec3 normal = glm::normalize(glm::cross(ab, ac));
+
+				a.normal = normal;
+				b.normal = normal;
+				c.normal = normal;
+
+				a.uv.x = a.pos.x;
+				a.uv.y = a.pos.y;
+				b.uv.x = b.pos.x;
+				b.uv.y = b.pos.y;
+				c.uv.x = c.pos.x;
+				c.uv.y = c.pos.y;
+			}
+
+			return vertexData;
 		}
 	}
 }
