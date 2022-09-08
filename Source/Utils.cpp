10ef31e55d606a19131d433d445603c430de03be
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-09-08 14:49:09 +0200

    ya

--- Source/Utils.cpp
+++ Source/Utils.cpp
@@ -6,12 +6,11 @@
 #include <string>
 #include <sstream>
 
-static void SubDivide(glm::vec3 &a, glm::vec3 &b, glm::vec3 &c, int recursions, std::vector<FLOOF::Vertex> &vertexData);
-static void pushVerts(glm::vec3 a, glm::vec3 b, glm::vec3 c, std::vector<FLOOF::Vertex> &vertexData);
-static glm::vec3 calcBarycentric(glm::vec3 position, glm::vec3 a, glm::vec3 b, glm::vec2 c);
 
 namespace FLOOF {
 	namespace Utils {
+        static void SubDivide(glm::vec3 &a, glm::vec3 &b, glm::vec3 &c, int recursions, std::vector<FLOOF::Vertex> &vertexData);
+        static void PushVerts(glm::vec3 a, glm::vec3 b, glm::vec3 c, std::vector<FLOOF::Vertex> &vertexData);
 		std::pair<std::vector<Vertex>, std::vector<uint32_t>> GetVisimVertexData(const std::string& path) {
 			std::vector<Vertex> vertexData;
 			std::vector<uint32_t> indexData;
@@ -90,74 +89,73 @@ namespace FLOOF {
 
             return { vertexData };
         }
-    }
-}
+        static void SubDivide(glm::vec3 &a, glm::vec3 &b, glm::vec3 &c, int recursions, std::vector<FLOOF::Vertex> &vertexData) {
+            if (recursions > 0) {
+                glm::vec3 v1 = glm::normalize(a + b);
+                glm::vec3 v2 = glm::normalize(a + c);
+                glm::vec3 v3 = glm::normalize(c + b);
 
-static void SubDivide(glm::vec3 &a, glm::vec3 &b, glm::vec3 &c, int recursions, std::vector<FLOOF::Vertex> &vertexData) {
-    if (recursions > 0) {
-        glm::vec3 v1 = glm::normalize(a + b);
-        glm::vec3 v2 = glm::normalize(a + c);
-        glm::vec3 v3 = glm::normalize(c + b);
+                SubDivide(a, v1, v2, recursions - 1, vertexData);
+                SubDivide(c, v2, v3, recursions - 1, vertexData);
+                SubDivide(b, v3, v1, recursions - 1, vertexData);
+                SubDivide(v3, v2, v1, recursions - 1, vertexData);
+            } else {
 
-        SubDivide(a, v1, v2, recursions - 1, vertexData);
-        SubDivide(c, v2, v3, recursions - 1, vertexData);
-        SubDivide(b, v3, v1, recursions - 1, vertexData);
-        SubDivide(v3, v2, v1, recursions - 1, vertexData);
-    } else {
+                PushVerts(a,b,c,vertexData);
+            }
+        }
 
-        pushVerts(a,b,c,vertexData);
-    }
-}
+        static void PushVerts(glm::vec3 a, glm::vec3 b, glm::vec3 c, std::vector<FLOOF::Vertex> &vertexData) {
+            glm::vec3 normal = glm::cross(b - a, c - a);
+            normal = glm::normalize(normal);
+            FLOOF::Vertex v{};
 
-static void pushVerts(glm::vec3 a, glm::vec3 b, glm::vec3 c, std::vector<FLOOF::Vertex> &vertexData) {
+            v.pos = a;
+            v.normal = normal;
+            vertexData.push_back(v);
 
-    glm::vec3 normal = glm::cross(b - a, c - a);
-    normal = glm::normalize(normal);
-    FLOOF::Vertex v{};
+            FLOOF::Vertex vertB{};
+            v.pos = b;
+            v.normal = normal;
+            vertexData.push_back(v);
 
-    v.pos = a;
-    v.normal = normal;
-    vertexData.push_back(v);
+            FLOOF::Vertex vertC{};
+            v.pos = c;
+            v.normal = normal;
+            vertexData.push_back(v);
+        }
 
-    FLOOF::Vertex vertB{};
-    v.pos = b;
-    v.normal = normal;
-    vertexData.push_back(v);
+        static glm::vec3 CalcBarycentric(glm::vec3 position, const Triangle& triangle) {
 
-    FLOOF::Vertex vertC{};
-    v.pos = c;
-    v.normal = normal;
-    vertexData.push_back(v);
-}
-
-static glm::vec3 calcBarycentric(glm::vec3 position, glm::vec3 a, glm::vec3 b, glm::vec2 c) {
-
-    glm::vec2 pos{position.x,position.y};
+            glm::vec2 pos{ position.x, position.z };
     
-    glm::vec2 p1{a.x,a.y};
-    glm::vec2 p2{b.x,b.y};
-    glm::vec2 p3{c.x,c.y};
+            glm::vec2 p1 = glm::vec2(triangle.A.pos.x, triangle.A.pos.z);
+            glm::vec2 p2 = glm::vec2(triangle.B.pos.x, triangle.B.pos.z);
+            glm::vec2 p3 = glm::vec2(triangle.C.pos.x, triangle.C.pos.z);
 
-    glm::vec2 Va = p2 - p1;
-    glm::vec2 Vb = p3 - p1;
-    glm::vec3 n = glm::cross(glm::vec3(Va.x, Va.y, 0), glm::vec3(Vb.x, Vb.y, 0));
-    float area = glm::length(n);
-    glm::vec3 cords;
+            glm::vec2 Va = p2 - p1;
+            glm::vec2 Vb = p3 - p1;
+            glm::vec3 n = glm::cross(glm::vec3(Va.x, Va.y, 0), glm::vec3(Vb.x, Vb.y, 0));
+            float area = glm::length(n);
+            glm::vec3 cords;
 
-    glm::vec2 p = p2 - pos;
-    glm::vec2 q = p3 - pos;
-    n = glm::cross(glm::vec3(p.x, p.y, 0), glm::vec3(q.x, q.y, 0));
-    cords.x = n.z / area;
+            glm::vec2 p = p2 - pos;
+            glm::vec2 q = p3 - pos;
+            n = glm::cross(glm::vec3(p.x, p.y, 0), glm::vec3(q.x, q.y, 0));
+            cords.x = n.z / area;
 
-    p = p3 - pos;
-    q = p1 - pos;
-    n = glm::cross(glm::vec3(p.x, p.y, 0), glm::vec3(q.x, q.y, 0));
-    cords.y = n.z / area;
+            p = p3 - pos;
+            q = p1 - pos;
+            n = glm::cross(glm::vec3(p.x, p.y, 0), glm::vec3(q.x, q.y, 0));
+            cords.y = n.z / area;
 
-    p = p1 - pos;
-    q = p2 - pos;
-    n = glm::cross(glm::vec3(p.x, p.y, 0), glm::vec3(q.x, q.y, 0));
-    cords.z = n.z / area;
+            p = p1 - pos;
+            q = p2 - pos;
+            n = glm::cross(glm::vec3(p.x, p.y, 0), glm::vec3(q.x, q.y, 0));
+            cords.z = n.z / area;
 
-    return cords;
+            return cords;
+        }
+    }
 }
+
