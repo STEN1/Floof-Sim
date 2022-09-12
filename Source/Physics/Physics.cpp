bfba67736fcc8b6e2c3ea61aa2ec834041db7c68
Adrevland <adrian.l.drevland@gmail.com>
2022-09-12 12:07:26 +0200

    TriangleBallIntersect

--- Source/Physics/Physics.cpp
+++ Source/Physics/Physics.cpp
@@ -1,8 +1,7 @@
-
-
 #include "Physics.h"
+#include "cmath"
 
 bool FLOOF::Physics::TriangleBallIntersect(const FLOOF::Triangle &triangle, const glm::vec3 &position, const float &radius) {
-    
-    return false;
+   auto intersect = std::abs(glm::dot(position-triangle.A,triangle.N));
+   return (intersect > radius);
 }
