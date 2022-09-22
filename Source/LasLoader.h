9deccc192baee455422e6409a57d313426ce5233
Mathias <51080320+Cliffback@users.noreply.github.com>
2022-09-22 10:43:54 +0200

    FLOOF-7 Refactoring

--- Source/LasLoader.h
+++ Source/LasLoader.h
@@ -11,12 +11,13 @@ public:
 private:
     std::vector<FLOOF::ColorVertex> VertexData;
     glm::vec3 offset{};
-    void Downscale(float multiplier);
 
-    void OffsetCenter();
+    void CalcCenter();
     void FindMinMax();
+    void UpdatePoints();
     glm::vec3 min{0.f};
     glm::vec3 max{0.f};
-
+    glm::vec3 middle{0.f};
+    float m_Scale{1.f};
 
 };
