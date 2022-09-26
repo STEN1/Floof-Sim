41ca683bd1068cfcd6a709ef76a696b6eef2191f
Adrevland <adrian.l.drevland@gmail.com>
2022-09-26 14:08:08 +0200

    FLOOF-7 smooth shade

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -99,8 +99,8 @@ namespace FLOOF {
            m_PointCloudEntity = m_Registry.create();
             m_Registry.emplace<PointCloudComponent>(m_PointCloudEntity, map.GetPointData());
 			auto [vData, iData] = map.GetIndexedData();
-			//m_Registry.emplace<MeshComponent>(m_PointCloudEntity, vData, iData); // smooth shade
-			m_Registry.emplace<MeshComponent>(m_PointCloudEntity, map.GetVertexData()); // flat shade
+			m_Registry.emplace<MeshComponent>(m_PointCloudEntity, vData, iData); // smooth shade
+			//m_Registry.emplace<MeshComponent>(m_PointCloudEntity, map.GetVertexData()); // flat shade
 			m_Registry.emplace<TextureComponent>(m_PointCloudEntity, "Assets/HappyTree.png");
 			m_Registry.emplace<TransformComponent>(m_PointCloudEntity);
             m_Registry.emplace<TerrainComponent>(m_PointCloudEntity,map.GetTriangles());
